44479292e62eb8eb7f06b3d3d2e72c9ac69a70d0
tresorsawasawa <tresorsawasawa@gmail.com>
2022-02-22 20:51:22 +0200

    Practice the 'ternary-operator' exercise

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,18 +3,33 @@ import React, { useState } from "react";
 // ternary operator
 
 const ShortCircuit = () => {
-  const [text, setText] = useState('')
-  const firstValue = text || 'hello world';
-  const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
+
+  const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
+
+  const errorStyling = {
+    color: "red",
+    paddingTop: "1.5rem",
+  };
 
   return (
     <>
       {/* <h2>{firstValue}</h2>
       <h2>Value: {secondValue}</h2> */}
 
-      <h2>{text || 'Tresor Sawasawa'}</h2>
-      {text && <h2>Hello From React</h2> }
-      {!text && <h2>Hello From React</h2> }
+      <h2>{text || "Tresor Sawasawa"}</h2>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {isError ? (
+        <h2 style={errorStyling}>Error...</h2>
+      ) : (
+        <div>
+          <h2>There is no error</h2>
+        </div>
+      )}
     </>
   );
 };

0b74e14b1560f6298f2803f112e8e9bc36f6999a
tresorsawasawa <tresorsawasawa@gmail.com>
2022-02-22 14:44:14 +0200

    Add a more complex counter

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -8,14 +8,28 @@ const UseStateCounter = () => {
   const handleDecrease = () => setValue(value - 1)
   const handleIncrease = () => setValue(value + 1)
 
+  const complexIncrease = () => {
+    setTimeout(() => {
+      setValue((prevSatate) => {
+        return prevSatate + 1
+      })
+    }, 1500)
+  }
+
   return (
     <>
       <section style={styling}>
         <h2>Regular Counter</h2>
         <h1>{value}</h1>
-        <button type="button" className="btn" onClick={handleDecrease}>decreament</button>
+        <button type="button" className="btn" onClick={handleDecrease}>decrease</button>
         <button type="button" className="btn" onClick={() => setValue(0)}>reset</button>
-        <button type="button" className="btn" onClick={handleIncrease}>increament</button>
+        <button type="button" className="btn" onClick={handleIncrease}>increase</button>
+      </section>
+
+      <section style={styling}>
+        <h2>More complex Counter</h2>
+        <h1>{value}</h1>
+        <button type="button" className="btn" onClick={complexIncrease}>increase later</button>
       </section>
     </>
   );

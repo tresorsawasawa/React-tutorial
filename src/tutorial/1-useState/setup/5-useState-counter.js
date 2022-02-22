f67e5119c0ae098533b62901e7a83947ae8d2f4e
tresorsawasawa <tresorsawasawa@gmail.com>
2022-02-22 14:31:20 +0200

    Create a simple 'Regular counter'

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const styling = { margin: '4rem 0'}
+
+  const [value, setValue] = useState(0);
+
+  const handleDecrease = () => setValue(value - 1)
+  const handleIncrease = () => setValue(value + 1)
+
+  return (
+    <>
+      <section style={styling}>
+        <h2>Regular Counter</h2>
+        <h1>{value}</h1>
+        <button type="button" className="btn" onClick={handleDecrease}>decreament</button>
+        <button type="button" className="btn" onClick={() => setValue(0)}>reset</button>
+        <button type="button" className="btn" onClick={handleIncrease}>increament</button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;

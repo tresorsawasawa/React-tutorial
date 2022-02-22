3b2550f7015f95c206355d2e7091306b1f622d60
tresorsawasawa <tresorsawasawa@gmail.com>
2022-02-22 16:30:07 +0200

    Complete the useEffect basics practice

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
-// second parameter
+// seco nd parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log("Call Use Efect");
+    if (value >= 1) {
+      document.title = `React Tutorial(${value})`;
+    }
+  }, [value]);
+
+  // useEffect(() => {
+  //   console.log("Hello World");
+  // });
+
+  console.log("Render component");
+
+  return (
+    <>
+      <h2>{value}</h2>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        click me
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;

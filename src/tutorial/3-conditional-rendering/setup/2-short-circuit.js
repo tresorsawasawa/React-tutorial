51570aa2ed872743d3a708973fef6b7e3e9d5481
tresorsawasawa <tresorsawasawa@gmail.com>
2022-02-22 20:32:55 +0200

    Practice the 'short-circuit' exercise

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState('')
+  const firstValue = text || 'hello world';
+  const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h2>{firstValue}</h2>
+      <h2>Value: {secondValue}</h2> */}
+
+      <h2>{text || 'Tresor Sawasawa'}</h2>
+      {text && <h2>Hello From React</h2> }
+      {!text && <h2>Hello From React</h2> }
+    </>
+  );
 };
 
 export default ShortCircuit;

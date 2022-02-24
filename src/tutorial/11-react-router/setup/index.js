80b860f726e3fd173fa3402bbbae90b48e183d70
tresorsawasawa <tresorsawasawa@gmail.com>
2022-02-24 13:13:31 +0200

    Practice 'Router' exercises

--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // react router
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 // pages
 import Home from './Home';
 import About from './About';
@@ -9,8 +9,20 @@ import Error from './Error';
 import Person from './Person';
 // navbar
 import Navbar from './Navbar';
+
 const ReactRouterSetup = () => {
-  return <h2>react router</h2>;
+  return (
+    <HashRouter>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/people" element={<People />} />
+        <Route path="/:id" element={<Person />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </HashRouter>
+  );
 };
 
 export default ReactRouterSetup;
